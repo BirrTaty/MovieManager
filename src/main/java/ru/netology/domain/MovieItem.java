84cea501539d24db89a517990f883f28159a49d3
Tatiana <birrtaty@gmail.com>
2021-11-12 23:07:54 +0300

    Java code

---
+++ src/main/java/ru/netology/domain/MovieItem.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class MovieItem {
+    private int id;
+    private String nameUrl;
+    private String genre;
+    private String image;
+}
---
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieManagerTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void getLastTen() {
+    }
+}
